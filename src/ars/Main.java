c6a63ebffb2eec4c067541f4f2d775ddd39d5225
Mohaned Yossry Abdulaziz <memo.yossry@gmail.com>
2018-10-08 21:46:44 +0200

    Login Design
    login screen desgin

---
+++ src/ars/Main.java
@@ -0,0 +1,53 @@
+package ars;
+
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
+
+        Scene scene = new Scene(root, 700, 400);
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/ars/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package ars.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class LoginController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("Hello");
+    }
+
+}
