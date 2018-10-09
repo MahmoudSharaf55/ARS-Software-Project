4ae9d6ba1b770f96d334f1fbdd8d0d5f95416198
Mohaned Yossry Abdulaziz <memo.yossry@gmail.com>
2018-10-09 12:58:37 +0200

    Changed App name to iFly added Exit Button

--- src/ars/controllers/LoginController.java
+++ src/ars/controllers/LoginController.java
@@ -1,14 +1,21 @@
 package ars.controllers;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class LoginController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        System.out.println("Hello");
+
     }
 
+    @FXML
+    public void exitAction() {
+        Platform.exit();
+    }+
 }
