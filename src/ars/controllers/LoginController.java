6ef171aa65676ccd6f4e4a2b1946ccc6262de750
Mohaned Yossry Abdulaziz <memo.yossry@gmail.com>
2018-10-10 08:00:11 +0200

    Added Signup button functionality

--- src/ars/controllers/LoginController.java
+++ src/ars/controllers/LoginController.java
@@ -1,13 +1,33 @@
 package ars.controllers;
 
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Window;
 
 public class LoginController implements Initializable {
+
+    private Stage mainStage;
+    @FXML
+    private AnchorPane ap;
+
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -17,5 +37,38 @@ public class LoginController implements Initializable {
     @FXML
     public void exitAction() {
         Platform.exit();
+    }
+
+    @FXML
+    public void onSignupClicked(ActionEvent ae) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/signup.fxml"));
+            Parent root;
+            root = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, 418, 562));
+            root.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    xOffset = event.getSceneX();
+                    yOffset = event.getSceneY();
+                }
+            });
+            root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    stage.setX(event.getScreenX() - xOffset);
+                    stage.setY(event.getScreenY() - yOffset);
+                }
+            });
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Node source = (Node) ae.getSource();
+        Stage theStage = (Stage) source.getScene().getWindow();
+
+        theStage.close();
     }
 }
