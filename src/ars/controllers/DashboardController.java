d4d39fcab1cc60b1ca52608d6b16a0a2686738ab
Mohaned Yossry Abdulaziz <memo.yossry@gmail.com>
2018-10-12 01:30:45 +0200

    Fixed

--- src/ars/controllers/DashboardController.java
+++ src/ars/controllers/DashboardController.java
@@ -1,4 +1,14 @@
 package ars.controllers;
 
-public class DashboardController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
 }
