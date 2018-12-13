72c81b923c8e31fb1f9cefa2d51119e2dd1baeb2
Mohammed $edky <mohammedsedky011472266@gmail.com>
2018-12-13 07:07:52 -0800

    complete Edit ticket

--- src/ars/controllers/UserEditingTicket.java
+++ src/ars/controllers/UserEditingTicket.java
@@ -20,4 +20,80 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
 
-public class UserEditingTicket {}
+public class UserEditingTicket  implements Initializable
+
+    {
+        /**
+         TODO: Sedky load flight info from flight like dashboard and active cancel button (Delete)
+         **/
+        @FXML
+        Label ticketNumber, source, dect, numFlight, price, date;
+        @FXML
+        JFXButton cancel;
+        @FXML
+        StackPane spTicket;
+
+        @FXML
+        void deleteTicket () {
+        try {
+            Connection deleteConnection = DBConnection.getConnection();
+            PreparedStatement deleteStatement = deleteConnection.prepareStatement("delete from ticket where user_id = ?");
+            deleteStatement.setInt(1, AuthUser.currentUser.getUserID());
+            deleteStatement.executeUpdate();
+            ticketNumber.setText("N/A");
+            numFlight.setText("N/A");
+            source.setText("N/A");
+            dect.setText("N/A");
+            date.setText("N/A");
+            price.setText("N/A");
+            UtilityServices.displayDialog(new Text("Confirm Cancel"), new Text("You are cancel Successfully...  "), spTicket);
+
+        } catch (SQLException ex) {
+
+            ex.printStackTrace();
+        }
+
+
+    }
+
+
+
+        @Override
+        public void initialize (URL location, ResourceBundle resources){
+        try {
+            Connection c = DBConnection.getConnection();
+            PreparedStatement statement = c.prepareStatement("select ticket_number,flight_number from ticket where user_id = ?");
+            PreparedStatement statement1 = c.prepareStatement("select src,dest,dateAndTime,price from flight where flightNumber = ?");
+            statement.setInt(1, AuthUser.currentUser.getUserID());
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String flightNumber = resultSet.getString("flight_number");
+
+                statement1.setString(1, flightNumber);
+                ResultSet flightResultSet = statement1.executeQuery();
+                if (flightResultSet.next()) {
+                    ticketNumber.setText(resultSet.getString("ticket_number"));
+                    numFlight.setText(flightNumber);
+                    source.setText(flightResultSet.getString("src"));
+                    dect.setText(flightResultSet.getString("dest"));
+                    DateFormat dateFormat = new SimpleDateFormat("dd/MM/YYYY HH:MM");
+                    date.setText(dateFormat.format(flightResultSet.getDate("dateAndTime")));
+                    price.setText(String.valueOf(flightResultSet.getInt("price")));
+                }
+                Ticket ticket = new Ticket(resultSet.getInt("ticket_number"), AuthUser.currentUser.getUserID(), flightNumber);
+            } else {
+                ticketNumber.setText("N/A");
+                numFlight.setText("N/A");
+                source.setText("N/A");
+                dect.setText("N/A");
+                date.setText("N/A");
+                price.setText("N/A");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    }
